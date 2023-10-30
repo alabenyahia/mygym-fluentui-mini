267dabb4564e7a60bb90616a62501c09079f6793
Ala ben yahia <alabinyahia@gmail.com>
2023-10-30 22:52:56 +0100

    Fix add new member form UI & functionality ( only showing toast left )

--- src/app/pages/manage/components/addbtnbody/Members.tsx
+++ src/app/pages/manage/components/addbtnbody/Members.tsx
@@ -1,4 +1,4 @@
-import { Label, Select, Field } from "@fluentui/react-components";
+import { Label, Dropdown, Field, Option } from "@fluentui/react-components";
 import useMemberships from "../../hooks/useMemberships";
 import { DatePicker } from "@fluentui/react-date-time";
 import { Formik, Form } from "formik";
@@ -7,10 +7,13 @@ import moment from "moment";
 import useMembers from "../../hooks/useMembers";
 import pb from "src/utils/db/pocketbase";
 import { FormikInput } from "../FormikInput";
+import { useState } from "react";
 
 export default function Members() {
   const { membershipsQuery } = useMemberships();
   const { memberMutation } = useMembers();
+  const [registeredDate, setRegisteredDate] = useState<Date>(new Date());
+  const [membershipId, setMembershipId] = useState("");
 
   return (
     <Formik
@@ -18,32 +21,30 @@ export default function Members() {
         name: "",
         email: "",
         phone: "",
-        registeredDate: "",
-        membershipId: "",
       }}
       validationSchema={Yup.object({
         name: Yup.string().required("Member name is required"),
       })}
       onSubmit={(values: any, { resetForm }) => {
         const membership = membershipsQuery.data?.find(
-          (element) => element.id === values.membershipId
+          (element) => element.id === membershipId
         );
 
-        let membershipExpirationDate = "";
+        let membershipExpirationDate: any = "";
         if (membership) {
           if (membership?.membershipType === "time") {
-            membershipExpirationDate = moment(values.registeredDate)
+            membershipExpirationDate = moment(registeredDate)
               .add(
                 membership?.timeQuantity,
                 membership?.timeType === "month" ? "months" : "days"
               )
-              .toString();
           }
         }
 
         console.log("valls", {
           ...values,
-          registeredDate: values.registeredDate.toString(),
+          registeredDate,
+          membershipId,
           membershipExpirationDate,
           assignedTo: pb.authStore.model?.id,
           deletedAt: "",
@@ -52,7 +53,8 @@ export default function Members() {
         memberMutation.mutate(
           {
             ...values,
-            registeredDate: values.registeredDate.toString(),
+            registeredDate,
+            membershipId,
             membershipExpirationDate,
             assignedTo: pb.authStore.model?.id,
             deletedAt: "",
@@ -64,71 +66,73 @@ export default function Members() {
                   name: "",
                   email: "",
                   phone: "",
-                  registeredDate: moment(),
-                  membershipId: "",
                 },
               });
+              setMembershipId("");
+              setRegisteredDate(new Date());
             },
           }
         );
       }}
     >
-      {({ values, handleChange }) => (
-        <Form id="add-form">
+      <Form id="add-form" style={{display: "flex", flexDirection: "column", gap: "8px"}}>
+        <div>
           <Label htmlFor="name" required>
             Member name
           </Label>
           <FormikInput placeholder="John Doe" name="name" id="name" />
+        </div>
 
+        <div>
           <Label htmlFor="email">Member email</Label>
           <FormikInput
             placeholder="example@email.com"
             name="email"
             id="email"
           />
+        </div>
 
+        <div>
           <Label htmlFor="phone">Member phone</Label>
           <FormikInput placeholder="23 111 222" name="phone" id="phone" />
+        </div>
 
+        <div>
           <Field label="Member registration date">
             <DatePicker
               placeholder="Select a date..."
-              name="registeredDate"
               id="registeredDate"
-              appearance="filled-darker"
-              value={values.registeredDate}
-              onSelectDate={handleChange}
+              value={registeredDate}
+              onSelectDate={(date) => setRegisteredDate(date!)}
+              showGoToToday={true}
             />
           </Field>
+        </div>
 
-          {membershipsQuery.data && membershipsQuery.data?.length > 0 ? (
-            <>
-              <label htmlFor="membershipId">Member membership</label>
-              <Select
-                name="membershipId"
-                id="membershipId"
-                value={values.membershipId}
-                onChange={handleChange}
-              >
-                {membershipsQuery.data?.map((membership: any) => {
-                  const currency = "TND";
-                  return (
-                    <option value={membership.id} key={membership.id}>
-                      <span style={{ fontWeight: "bold" }}>
-                        {membership.name}
-                      </span>
-                      <span>
-                        ({membership.price}
-                        {currency})
-                      </span>
-                    </option>
-                  );
-                })}
-              </Select>
-            </>
-          ) : null}
-        </Form>
-      )}
+        {membershipsQuery.data && membershipsQuery.data?.length > 0 ? (
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <label htmlFor="membershipId">Member membership</label>
+            <Dropdown
+              name="membershipId"
+              id="membershipId"
+              onOptionSelect={(_, data) =>
+                setMembershipId(data.optionValue as string)
+              }
+            >
+              {membershipsQuery.data?.map((membership: any) => {
+                const currency = "TND";
+                return (
+                  <Option
+                    text={`${membership.name} ${membership.price}${currency}`}
+                    value={membership.id}
+                    key={membership.id}
+                  >{`${membership.name} ${membership.price}${currency}`}</Option>
+                );
+              })}
+            </Dropdown>
+          </div>
+        ) : null}
+      </Form>
     </Formik>
   );
 }
