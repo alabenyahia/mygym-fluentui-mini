e5074d95857be75fa4f9eb1542bb9d6b5c690966
Ala ben yahia <alabinyahia@gmail.com>
2023-10-31 15:29:36 +0100

    Format dates in manage members table

--- src/app/pages/manage/Members.tsx
+++ src/app/pages/manage/Members.tsx
@@ -3,13 +3,27 @@ import { membersColumns } from "./data/main";
 import { Toaster } from "@fluentui/react-components";
 import useMembers from "./hooks/useMembers";
 import AddBtnBody from "./components/addbtnbody/Members";
+import moment from "moment";
 
 export default function Members() {
   const { membersQuery, membersToasterId } = useMembers();
 
+  const tableData = membersQuery.data
+    ? membersQuery.data.map((member) => {
+        return {
+          ...member,
+          membershipExpirationDate: member.membershipExpirationDate
+            ? moment(member.membershipExpirationDate).format("LL")
+            : "",
+          registeredDate: member.registeredDate
+            ? moment(member.registeredDate).format("LL")
+            : "",
+        };
+      })
+    : [];
   const membersProps = {
     tableColumns: membersColumns,
-    tableData: membersQuery.data || [],
+    tableData,
     tableLoading: membersQuery.isFetching || membersQuery.isLoading,
     tableLoadingText: "Loading members...",
     breadcrumbItems: ["Manage", "Members"],
