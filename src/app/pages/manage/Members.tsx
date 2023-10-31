efda29ef18b3719f636981ac7ce2c5fca9d97bd1
Ala ben yahia <alabinyahia@gmail.com>
2023-10-31 14:39:10 +0100

    Show loading spinner on add member loading

--- src/app/pages/manage/Members.tsx
+++ src/app/pages/manage/Members.tsx
@@ -3,39 +3,9 @@ import { membersColumns } from "./data/main";
 import { Toaster } from "@fluentui/react-components";
 import useMembers from "./hooks/useMembers";
 import AddBtnBody from "./components/addbtnbody/Members";
-import {useState} from "react";
-import moment from "moment";
 
 export default function Members() {
-  const { memberMutation, membersQuery, membersToasterId } = useMembers();
-  // const [name, setName] = useState("")
-  // const [email, setEmail] = useState("")
-  // const [phone, setPhone] = useState("")
-  // const [registeredDate, setRegisteredDate] = useState(moment()) //convert it to string when sending to db
-  // const [membershipId, setMembershipId] = useState("")
-
-  // const data = {
-  //   name: {
-  //     value: name,
-  //     setValue: setName,
-  //   },
-  //   email: {
-  //     value: email,
-  //     setValue: setEmail,
-  //   },
-  //   phone: {
-  //     value: phone,
-  //     setValue: setPhone,
-  //   },
-  //   registeredDate: {
-  //     value: registeredDate,
-  //     setValue: setRegisteredDate,
-  //   },
-  //   membershipId: {
-  //     value: membershipId,
-  //     setValue: setMembershipId,
-  //   },
-  // }
+  const { membersQuery, membersToasterId } = useMembers();
 
   const membersProps = {
     tableColumns: membersColumns,
--- src/app/pages/manage/components/addbtnbody/Members.tsx
+++ src/app/pages/manage/components/addbtnbody/Members.tsx
@@ -1,4 +1,11 @@
-import { Label, Dropdown, Field, Option, Toaster } from "@fluentui/react-components";
+import {
+  Label,
+  Dropdown,
+  Field,
+  Option,
+  Toaster,
+  Spinner,
+} from "@fluentui/react-components";
 import useMemberships from "../../hooks/useMemberships";
 import { DatePicker } from "@fluentui/react-date-time";
 import { Formik, Form } from "formik";
@@ -33,11 +40,10 @@ export default function Members() {
         let membershipExpirationDate: any = "";
         if (membership) {
           if (membership?.membershipType === "time") {
-            membershipExpirationDate = moment(registeredDate)
-              .add(
-                membership?.timeQuantity,
-                membership?.timeType === "month" ? "months" : "days"
-              )
+            membershipExpirationDate = moment(registeredDate).add(
+              membership?.timeQuantity,
+              membership?.timeType === "month" ? "months" : "days"
+            );
           }
         }
 
@@ -75,63 +81,74 @@ export default function Members() {
         );
       }}
     >
-      <Form id="add-form" style={{display: "flex", flexDirection: "column", gap: "8px"}}>
-        <div>
-          <Label htmlFor="name" required>
-            Member name
-          </Label>
-          <FormikInput placeholder="John Doe" name="name" id="name" />
-        </div>
-
-        <div>
-          <Label htmlFor="email">Member email</Label>
-          <FormikInput
-            placeholder="example@email.com"
-            name="email"
-            id="email"
-          />
-        </div>
-
-        <div>
-          <Label htmlFor="phone">Member phone</Label>
-          <FormikInput placeholder="23 111 222" name="phone" id="phone" />
-        </div>
-
-        <div>
-          <Field label="Member registration date">
-            <DatePicker
-              placeholder="Select a date..."
-              id="registeredDate"
-              value={registeredDate}
-              onSelectDate={(date) => setRegisteredDate(date!)}
-              showGoToToday={true}
-            />
-          </Field>
-        </div>
-
-        {membershipsQuery.data && membershipsQuery.data?.length > 0 ? (
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            <label htmlFor="membershipId">Member membership</label>
-            <Dropdown
-              name="membershipId"
-              id="membershipId"
-              onOptionSelect={(_, data) =>
-                setMembershipId(data.optionValue as string)
-              }
-            >
-              {membershipsQuery.data?.map((membership: any) => {
-                const currency = "TND";
-                return (
-                  <Option
-                    text={`${membership.name} ${membership.price}${currency}`}
-                    value={membership.id}
-                    key={membership.id}
-                  >{`${membership.name} ${membership.price}${currency}`}</Option>
-                );
-              })}
-            </Dropdown>
+      <Form
+        id="add-form"
+        style={{ display: "flex", flexDirection: "column", gap: "8px" }}
+      >
+        {memberMutation.isPending ? (
+          <div>
+            <Spinner label="Adding member..." />
           </div>
-        ) : null}
+        ) : (
+          <>
+            <div>
+              <Label htmlFor="name" required>
+                Member name
+              </Label>
+              <FormikInput placeholder="John Doe" name="name" id="name" />
+            </div>
+
+            <div>
+              <Label htmlFor="email">Member email</Label>
+              <FormikInput
+                placeholder="example@email.com"
+                name="email"
+                id="email"
+              />
+            </div>
+
+            <div>
+              <Label htmlFor="phone">Member phone</Label>
+              <FormikInput placeholder="23 111 222" name="phone" id="phone" />
+            </div>
+
+            <div>
+              <Field label="Member registration date">
+                <DatePicker
+                  placeholder="Select a date..."
+                  id="registeredDate"
+                  value={registeredDate}
+                  onSelectDate={(date) => setRegisteredDate(date!)}
+                  showGoToToday={true}
+                />
+              </Field>
+            </div>
+
+            {membershipsQuery.data && membershipsQuery.data?.length > 0 ? (
+              <div style={{ display: "flex", flexDirection: "column" }}>
+                <label htmlFor="membershipId">Member membership</label>
+                <Dropdown
+                  name="membershipId"
+                  id="membershipId"
+                  onOptionSelect={(_, data) =>
+                    setMembershipId(data.optionValue as string)
+                  }
+                >
+                  {membershipsQuery.data?.map((membership: any) => {
+                    const currency = "TND";
+                    return (
+                      <Option
+                        text={`${membership.name} ${membership.price}${currency}`}
+                        value={membership.id}
+                        key={membership.id}
+                      >{`${membership.name} ${membership.price}${currency}`}</Option>
+                    );
+                  })}
+                </Dropdown>
+              </div>
+            ) : null}
+          </>
+        )}
         <Toaster toasterId={membersToasterId} />
       </Form>
     </Formik>
